459bd50a86b2441e6a69abff2c4e13c3086ee185
Hossam Limouni <limouni.hossam@gmail.com>
2022-06-03 17:30:31 +0100

    socketsPool

--- SocketsPool.cpp
+++ SocketsPool.cpp
@@ -7,6 +7,7 @@ SocketsPool::SocketsPool()
     FD_ZERO(&this->_fdPool);
     FD_ZERO(&this->_readFds);
     FD_ZERO(&this->_writeFds);
+    FD_ZERO(&this->_exepFds);
 }
 
 SocketsPool::SocketsPool(listeningSocket const & listenSock)
@@ -16,13 +17,49 @@ SocketsPool::SocketsPool(listeningSocket const & listenSock)
     FD_ZERO(&this->_fdPool);
     FD_ZERO(&this->_readFds);
     FD_ZERO(&this->_writeFds);
+    FD_ZERO(&this->_exepFds);
     FD_SET(listenSock.get_fd(), &this->_fdPool);
     this->_descriptors.insert(listenSock.get_fd());
 }
 
 void SocketsPool::addToRead(wsv::Socket const & sock)
 {
-    FD_SET(sock.get_fd(), )
+    FD_SET(sock.get_fd(), &this->_readFds);
+    this->_descriptors.insert(sock.get_fd());
+}
+
+void SocketsPool::addToWrite(wsv::Socket const & sock)
+{
+    FD_SET(sock.get_fd(), &this->_writeFds);
+    this->_descriptors.insert(sock.get_fd());
+}
+
+int SocketsPool::getMaxFd() const
+{
+    return *this->_descriptors.begin();
+}
+
+bool SocketsPool::isReadable(wsv::Socket const & sock) const
+{
+    for (int i = 0; i < getMaxFd() + 1; i++)
+    {
+        /* code */
+    }
+    
+}
+
+int SocketsPool::checkActivity()
+{
+    fd_set readSet, writeSet, excepSet;
+
+    FD_ZERO(&readSet);
+    FD_ZERO(&writeSet);
+    FD_ZERO(&excepSet);
+    FD_COPY(&this->_readFds, &readSet);
+    FD_COPY(&this->_writeFds, &writeSet);
+    FD_COPY(&this->_exepFds, &excepSet);
+    int activity = select(getMaxFd() + 1, &readSet, &writeSet, &excepSet, &this->_timeout);
+    return activity;
 }
 
 SocketsPool::~SocketsPool()
