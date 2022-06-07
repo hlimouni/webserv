a6a0ab2aeed6517542add5e4edb7066df7205fa6
Hossam Limouni <hlimouni@e3r12p5.1337.ma>
2022-06-07 11:28:23 +0100

    modifying initsets function to accept map as parameter instead of set

--- SocketsPool.cpp
+++ SocketsPool.cpp
@@ -8,19 +8,19 @@ SocketsPool::SocketsPool()
     FD_ZERO(&this->_exepFds);
 }
 
-void SocketsPool::addToRead(wsv::Socket const & sock)
+void SocketsPool::addToRead(wsv::Socket const &sock)
 {
     FD_SET(sock.get_fd(), &this->_readFds);
     this->_descriptors.insert(sock.get_fd());
 }
 
-void SocketsPool::addToWrite(wsv::Socket const & sock)
+void SocketsPool::addToWrite(wsv::Socket const &sock)
 {
     FD_SET(sock.get_fd(), &this->_writeFds);
     this->_descriptors.insert(sock.get_fd());
 }
 
-void SocketsPool::addToExept(wsv::Socket const & sock)
+void SocketsPool::addToExept(wsv::Socket const &sock)
 {
     FD_SET(sock.get_fd(), &this->_exepFds);
     this->_descriptors.insert(sock.get_fd());
@@ -31,32 +31,38 @@ int SocketsPool::getMaxFd() const
     return *this->_descriptors.begin();
 }
 
-bool SocketsPool::isReadable(wsv::Socket const & sock)
+bool SocketsPool::isReadable(wsv::Socket const &sock)
 {
     if (FD_ISSET(sock.get_fd(), &this->_readFds))
         return true;
     return false;
 }
 
-bool SocketsPool::isWriteable(wsv::Socket const & sock)
+bool SocketsPool::isWriteable(wsv::Socket const &sock)
 {
     if (FD_ISSET(sock.get_fd(), &this->_writeFds))
         return true;
     return false;
 }
 
-void SocketsPool::initSets(std::set<listeningSocket> listenSockets, std::list<clientData> clients)
+void SocketsPool::initSets(std::map<std::string, std::set<listeningSocket> > hostSockets, std::list<clientData> clients)
 {
     FD_ZERO(&_readFds);
     FD_ZERO(&_writeFds);
     FD_ZERO(&_exepFds);
 
-    for (std::set<listeningSocket>::iterator it_sock = listenSockets.begin(); it_sock != listenSockets.end(); it_sock++)
+    std::map<std::string, std::set<listeningSocket> >::iterator it;
+    for (it = hostSockets.begin(); it != hostSockets.end(); it++)
     {
-        // FD_SET((*it_sock).get_fd(), &_readFds);
-        // FD_SET((*it_sock).get_fd(), &_exepFds);
-        addToRead(*it_sock);
-        addToExept(*it_sock);
+        std::set<listeningSocket> sockSet = it->second;
+
+        for (std::set<listeningSocket>::iterator it_sock = sockSet.begin(); it_sock != sockSet.end(); it_sock++)
+        {
+            // FD_SET((*it_sock).get_fd(), &_readFds);
+            // FD_SET((*it_sock).get_fd(), &_exepFds);
+            addToRead(*it_sock);
+            addToExept(*it_sock);
+        }
     }
     for (std::list<clientData>::iterator it_client = clients.begin(); it_client != clients.end(); it_client++)
     {
@@ -65,20 +71,20 @@ void SocketsPool::initSets(std::set<listeningSocket> listenSockets, std::list<cl
             addToWrite((*it_client).GetSocket());
         else
             // FD_SET((*it_client).GetSocketFd(), &_readFds);
-			addToRead((*it_client).GetSocket());
+            addToRead((*it_client).GetSocket());
         // FD_SET((*it_client).GetSocketFd(), &_exepFds);
-		addToExept((*it_client).GetSocket());
+        addToExept((*it_client).GetSocket());
     }
 }
 
 int SocketsPool::checkActivity()
 {
     int activity = select(getMaxFd() + 1, &_readFds, &_writeFds, &_exepFds,
-                    &this->_timeout);
+                          &this->_timeout);
     return activity;
 }
 
-void SocketsPool::removeFromRead(wsv::Socket const & sock)
+void SocketsPool::removeFromRead(wsv::Socket const &sock)
 {
     FD_CLR(sock.get_fd(), &this->_readFds);
 }
